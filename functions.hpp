1f674ff587b55f8687987df5f63c681e1bd440bf
hedniskhjartad <jonatanvc.1997@gmail.com>
2026-06-04 18:18:16 -0400

    Creada una calculadora

---
+++ functions.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+#include <iostream>
+
+int addition(int x, int y);
+int subtraction(int x, int y);
+int multiplication(int x, int y);
+int division(int x, int y);
--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
-#include <stdio.h>
+#include <iostream>
+#include <limits>
+#include "functions.hpp"
+
+
+using std::cin, std::cout;
 
 int main(){
+    
+    int operation;
+    cout << "Choose an operation\n\n";
+    cout << "1 - Addition\n";
+    cout << "2 - Subtraction\n";
+    cout << "3 - Multiplication\n";
+    cout << "4 - Division\n";
+    cin >> operation;
 
-    printf("Hello world");
+    int number1;
+    int number2;
+    
+    cout << "Type 1st number : ";
+    if(!(cin >> number1)){
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    }
+    cout << "\nType 2nd number : ";
+    if(!(cin >> number2)){
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    }
 
+    switch (operation)
+    {
+    case 1: cout << "Result = " << addition(number1,number2);break;
+    case 2: cout << "Result = " << subtraction(number1,number2);break;
+    case 3: cout << "Result = " << multiplication(number1,number2);break;
+    case 4: cout << "Result = " << division(number1,number2);break;
+
+    default:
+        cout << "Error, invalid value\n";
+        break;
+    }
+    
 return 0;
 }
