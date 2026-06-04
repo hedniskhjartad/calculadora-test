185ec609f3669deb9ce19bc41f59d1b97cb922d2
hedniskhjartad <jonatanvc.1997@gmail.com>
2026-06-04 18:36:20 -0400

    Añadido Loop

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include <limits>
 #include "functions.hpp"
 
-
 using std::cin, std::cout;
 
 int main(){
-    
+
     int operation;
+
+    while (operation != 5){
     cout << "Choose an operation\n\n";
-    cout << "1 - Addition\n";
-    cout << "2 - Subtraction\n";
-    cout << "3 - Multiplication\n";
-    cout << "4 - Division\n";
+    cout << "1. +\n";
+    cout << "2. -\n";
+    cout << "3. x\n";
+    cout << "4. /\n";
+    cout << "5. Exit\n";
     cin >> operation;
+    if (operation == 5){return 0;}
+    
 
     int number1;
     int number2;
@@ -31,15 +35,15 @@ int main(){
 
     switch (operation)
     {
-    case 1: cout << "Result = " << addition(number1,number2);break;
-    case 2: cout << "Result = " << subtraction(number1,number2);break;
-    case 3: cout << "Result = " << multiplication(number1,number2);break;
-    case 4: cout << "Result = " << division(number1,number2);break;
+    case 1: cout << "Result = " << addition(number1,number2) << "\n\n" ;break;
+    case 2: cout << "Result = " << subtraction(number1,number2) << "\n\n";break;
+    case 3: cout << "Result = " << multiplication(number1,number2) << "\n\n";break;
+    case 4: cout << "Result = " << division(number1,number2) << "\n\n";break;
 
-    default:
-        cout << "Error, invalid value\n";
-        break;
+    default:cout << "Error, invalid value\n";break;
     }
     
+    }
+
 return 0;
 }
